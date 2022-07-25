b394db90916c5d1faa6eff830a5ee5fcfe385233
VanessaGuzman12 <100581227+VanessaGuzman12@users.noreply.github.com>
2022-07-25 17:41:56 +0800

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -21,7 +21,7 @@ def index():
     graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
 
 
-    #Barchart disease
+    #Barchart gender
     graph2_df=df.groupby(["gender"]).count().sort_values(["date_regis"],ascending=False)
     graph2_df.reset_index(inplace=True)
     fig2 = px.bar(graph2_df, x="gender", y=['date_regis'], title = "Deaths by gender in Mexico", color="gender")
@@ -32,5 +32,17 @@ def index():
     fig3 = px.histogram(df, x="death_age", nbins= 20,color="gender", title = "Deaths by age in Mexico")
     graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template("index.html", title ="Home", graph1JSON = graph1JSON, graph2JSON = graph2JSON, graph3JSON = graph3JSON)
+    #histogram Covid19
+    graph4_df=df.drop(df[df.death_cause_eng!="Covid-19"].index)
+    fig4 = px.histogram(graph4_df, x="death_age", nbins= 20,color="gender", title = "Deaths by Covid-19 by age in Mexico")
+    graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    #histogram External causes
+    graph5_df=df.drop(df[df.death_cause_eng!="External causes"].index)
+    fig5 = px.histogram(graph5_df, x="death_age", nbins= 20,color="gender", title = "Deaths by External Causes by age in Mexico")
+    graph5JSON = json.dumps(fig5, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    return render_template("index.html", title ="Home", graph1JSON = graph1JSON, graph2JSON = graph2JSON, graph3JSON = graph3JSON, graph4JSON = graph4JSON, graph5JSON = graph5JSON)
 
